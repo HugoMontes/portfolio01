acc46502d3255399e4ac1f284f7884fcb8babbd3
HugoMontes <hugomontes927@gmail.com>
2021-06-25 13:58:33 -0400

    Inicio del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ExperiencesComponent } from './experiences/experiences.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { SkillsComponent } from './skills/skills.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    SidebarComponent,
+    ExperiencesComponent,
+    ProjectsComponent,
+    SkillsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
