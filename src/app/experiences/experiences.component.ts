e78efe3c46b18537d2c72ef325d691448acd63ab
HugoMontes <hugomontes927@gmail.com>
2021-06-25 18:13:04 -0400

    Lista de Trabajos. Inyección de dependencia se un servicio en el constructor

--- src/app/experiences/experiences.component.ts
+++ src/app/experiences/experiences.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IJob } from './job';
+import { JobService } from '../services/job.service';
 
 @Component({
   selector: 'app-experiences',
@@ -8,31 +9,15 @@ import { IJob } from './job';
 })
 export class ExperiencesComponent implements OnInit {
 
-  public title = 'EXPERIENCES';
+  public title : string
+  public jobs : IJob[];
 
-  constructor() { }
+  constructor(private jobService : JobService) { 
+    this.title = 'EXPERIENCES'
+    this.jobs = this.jobService.getJobs();
+  }
 
   ngOnInit(): void {
   }
-
-  jobs : IJob[] = [
-    {
-      "title": "Lead Developer",
-      "time": "2019 - Present",
-      "company": "Startup Hubs, San Francisco",
-      "details": "Describe your role here lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo."
-    },
-    {
-      "title": "Senior Software Engineer",
-      "time": "2018 - 2019",
-      "company": "Google, London",
-      "details": "Describe your role here lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem."
-    },
-    {
-      "title": "UI Developer",
-      "time": "2016 - 2018",
-      "company": "Amazon, London",
-      "details": "Describe your role here lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem."   
-    }
-  ];
+    
 }
